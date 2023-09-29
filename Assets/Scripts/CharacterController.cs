91957c6fbe1e0673b3e91960ba845b295355eaac
hugge <hugo.larsson@elev.ga.lbs.se>
2023-09-29 10:39:53 +0200

    Gravitation, hoppa fungerar

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,35 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+
+public enum CharacterState // uppräknade lista av karaktär tillstånd
+{
+    Grounded = 0,
+    Airborne = 1,
+    Jumping = 2,
+    Total
+
+}
 
 public class CharacterController : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
 
+    public CharacterState JumpingState = CharacterState.Airborne; // är karaktären på marken eller i luften?
 
-    public float MovementSpeedPerSecond = 10.0f; // gå
+ 
+
+    public float MovementSpeedPerSecond = 120.0f; // gå
     public float GravityPerSecond = 140.0f; // falla
     public float GroundLevel = 0.0f; // grunden
+
+    //hoppa
+    public float JumpSpeedFactor = 3.0f; // Hur mycket snabbare hoppet är
+    public float JumpMaxHeight = 150;
+    private float JumpHeightDelta = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
-        // Gravitation
-        Vector3 gravityPosition = transform.position;
-        gravityPosition.y -= GravityPerSecond * Time.deltaTime;
-        if(gravityPosition.y < GroundLevel) { gravityPosition.y = GroundLevel; }
-        transform.position = gravityPosition;
-
-
-        if (Input.GetKey(KeyCode.W)) // up
+        if (transform.position.y <= GroundLevel)
         {
             Vector3 characterPosition = transform.position;
-            characterPosition.y += MovementSpeedPerSecond * Time.deltaTime;
+            characterPosition.y = GroundLevel;
             transform.position = characterPosition;
+            JumpingState = CharacterState.Grounded;
         }
+
+
+        if (Input.GetKey(KeyCode.W) && JumpingState == CharacterState.Grounded) // up
+        {
+            JumpingState = CharacterState.Jumping;
+            JumpHeightDelta = 0.0f;
+
+        }
+        if (JumpingState == CharacterState.Jumping)
+        {
+            Vector3 characterPosition = transform.position;
+            float totalJumpMovementThisFrame = MovementSpeedPerSecond * JumpSpeedFactor * Time.deltaTime;
+            characterPosition.y += totalJumpMovementThisFrame;
+            transform.position = characterPosition;
+            JumpHeightDelta += totalJumpMovementThisFrame;
+            if(JumpHeightDelta >= JumpMaxHeight)
+            {
+                JumpingState = CharacterState.Airborne;
+                JumpHeightDelta = 0.0f;
+            }
+        }
+
         if (Input.GetKey(KeyCode.S)) // ner
         {
             Vector3 characterPosition = transform.position;
@@ -48,6 +76,12 @@ public class CharacterController : MonoBehaviour
             characterPosition.x += MovementSpeedPerSecond * Time.deltaTime;
             transform.position = characterPosition;
         }
-
+        {
+            // Gravitation
+            Vector3 gravityPosition = transform.position;
+            gravityPosition.y -= GravityPerSecond * Time.deltaTime;
+            if (gravityPosition.y < GroundLevel) { gravityPosition.y = GroundLevel; }
+            transform.position = gravityPosition;
+        }
     }
 }
